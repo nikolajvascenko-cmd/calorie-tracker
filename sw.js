bd47835b5f7b516c8c8df93a7a3046a99df61140
nikolajvascenko-cmd <nikolajvascenko@gmail.com>
2026-06-11 12:34:00 +0100

    Fix icon file paths in service worker

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE_NAME = "calorie-track-v1";
-const FILES = ["./", "./index.html", "./styles.css", "./app.js", "./manifest.json", "./icons/icon-180.png", "./icons/icon-192.png", "./icons/icon-512.png"];
+const FILES = ["./", "./index.html", "./styles.css", "./app.js", "./manifest.json", "./icon-180.png", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(FILES)));
